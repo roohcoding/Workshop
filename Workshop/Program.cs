c9070567e4abcab0d943881d92ce4e0035068660
Rupali Natewad <rupalirnatewad@gmail.com>
2022-05-18 09:45:39 +0530

    Add project files.

---
+++ Workshop/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Workshop;
+
+Console.WriteLine("Hello, World!");
+SnakeRandom.Snake();
---
+++ Workshop/SnakeRandom.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop
+{
+    internal class SnakeRandom
+    {
+        public static int num1;
+        public static void Snake()
+        {
+            Random obj1 = new Random();
+            num1 = obj1.Next(1, 7); 
+
+
+        if (num1 == 1)
+            {
+                Console.WriteLine("U got" + num1);
+            }
+            else if (num1 == 2)
+            {
+                Console.WriteLine("U got" + num1);
+
+            }
+            else if (num1 == 3)
+            {
+                Console.WriteLine("U got" + num1);
+
+            }
+            else if (num1 == 4)
+            {
+                Console.WriteLine("U got" + num1);
+            }
+            else if (num1 == 5)
+            {
+                Console.WriteLine("U got" + num1);
+
+            }
+            else if (num1 == 6)
+            {
+                Console.WriteLine("U got" + num1);
+            }
+            else
+            {
+                Console.WriteLine("U got Nothing");
+            }
+
+        }
+    }
+}
